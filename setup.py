21fecb5fa58699f4f3f8e8b991a70058717e2f3b
Anjali Jaiswal <anjalirjaiswal@gmail.com>
2025-11-01 07:06:05 +0530

    This is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='anjali',
+    author_email='anjalirjaiswal@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
